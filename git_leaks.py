09148d7ac6d3d02d329883e5bf2cba88216a344c
nacho <202105547@alu.comillas.edu>
2022-10-20 19:36:08 +0200

    final_version1

--- git_leaks.py
+++ git_leaks.py
@@ -3,10 +3,6 @@ import re, signal, sys, time
 from git import Repo
 
 END = "\033[m"
-RED = "\033[38;5;196m"
-GREEN = "\033[38;5;40m"
-YELLOW = "\033[38;5;226m"
-
 
 REPO_DIR = "1.1gitleaks/skale/skale-manager"
 leaks = ['key', 'password', 'credentials']
@@ -17,7 +13,7 @@ def handler_signal(signal, frame):
     sys.exit(1)
 
 
-def compilar_patrones(leaks):
+def compile_p(leaks):
     compiled_leaks = []
     for leak in leaks:
         compiled_leaks.append(re.compile(leak))
@@ -64,10 +60,12 @@ def progress_bar(iz, de):
 if __name__ == "__main__":
 
     signal.signal(signal.SIGINT, handler_signal)
-    compiled_leaks = compilar_patrones(leaks)
+
+    compiled_leaks = compile_p(leaks)
     repo = extract(REPO_DIR)
     commit_list,message_list = transform(repo, compiled_leaks)
     progress_bar(1, 66)
     progress_bar(67, 124)
     load(commit_list, message_list)
     progress_bar(124, 200)
+    exit(0)
